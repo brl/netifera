4cf857d3ed60202c109e780a19e235a6752cad44
Luciano Notarfrancesco <luciano@netifera.com>
2009-09-05 11:20:50 +0800

    improved top-10 passwords list

--- platform/com.netifera.platform.net/com.netifera.platform.net.wordlists/src/com/netifera/platform/net/wordlists/examples/Top10Passwords.java
+++ platform/com.netifera.platform.net/com.netifera.platform.net.wordlists/src/com/netifera/platform/net/wordlists/examples/Top10Passwords.java
@@ -15,7 +15,8 @@ public class Top10Passwords implements IWordList {
 
 	public FiniteIterable<String> getWords() {
 		List<String> words = new ArrayList<String>();
-		for (String word: "123 password liverpool letmein 123456 qwerty charlie monkey arsenal thomas".split(" "))
+//		for (String word: "123 password liverpool letmein 123456 qwerty charlie monkey arsenal thomas".split(" "))
+		for (String word: "123 password 123456 qwerty letmein monkey password1 abc123 12345678 1234".split(" "))
 			words.add(word);
 		return new ListIndexedIterable<String>(words);
 	}
