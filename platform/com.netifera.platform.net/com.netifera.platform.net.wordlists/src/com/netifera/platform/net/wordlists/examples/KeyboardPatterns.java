a87f6c05ecb8a28b6279d4d9f8168616058118ea
Luciano Notarfrancesco <luciano@netifera.com>
2009-10-08 20:28:12 +0800

    Improved 'keyboard patterns' wordlist

--- platform/com.netifera.platform.net/com.netifera.platform.net.wordlists/src/com/netifera/platform/net/wordlists/examples/KeyboardPatterns.java
+++ platform/com.netifera.platform.net/com.netifera.platform.net.wordlists/src/com/netifera/platform/net/wordlists/examples/KeyboardPatterns.java
@@ -10,7 +10,7 @@ import com.netifera.platform.net.wordlists.IWordList;
 public class KeyboardPatterns implements IWordList {
 
 	public String getName() {
-		return "Keyboard Patterns (21)";
+		return "Keyboard Patterns (28)";
 	}
 
 	public String getCategory() {
@@ -19,7 +19,7 @@ public class KeyboardPatterns implements IWordList {
 
 	public FiniteIterable<String> getWords() {
 		List<String> words = new ArrayList<String>();
-		for (String word: "123 123456 qwerty abc123 12345678 1234 asdf 159357 1qaz2wsx 1q2w3e 1234qwer zaq1xsw2 zxcvbnm abcd1234 a1b2c3d4 zxcvasdf zaxscd qeadzc !@#$ !@#$%^&* !Q@W#E".split(" "))
+		for (String word: "123 123456 qwerty abc123 12345678 1234 asdf 159357 123456789 1234567 111111 654321 777777 666666 555555 1qaz2wsx 1q2w3e 1234qwer zaq1xsw2 zxcvbnm abcd1234 a1b2c3d4 zxcvasdf zaxscd qeadzc !@#$ !@#$%^&* !Q@W#E".split(" "))
 			words.add(word);
 		return new ListIndexedIterable<String>(words);
 	}
