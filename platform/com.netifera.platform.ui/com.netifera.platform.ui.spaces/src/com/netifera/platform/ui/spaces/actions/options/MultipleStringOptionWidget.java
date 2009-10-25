6ffba9ea4720fb00671cbf2a34a8cc69e6155fd5
Luciano Notarfrancesco <luciano@netifera.com>
2009-10-25 18:53:33 +0800

    More beautiful multiple string options, used for example for wordlists in the hover

--- platform/com.netifera.platform.ui/com.netifera.platform.ui.spaces/src/com/netifera/platform/ui/spaces/actions/options/MultipleStringOptionWidget.java
+++ platform/com.netifera.platform.ui/com.netifera.platform.ui.spaces/src/com/netifera/platform/ui/spaces/actions/options/MultipleStringOptionWidget.java
@@ -1,110 +1,60 @@
 package com.netifera.platform.ui.spaces.actions.options;
 
-import org.eclipse.jface.viewers.CheckboxTableViewer;
-import org.eclipse.jface.viewers.ColumnWeightData;
-import org.eclipse.jface.viewers.ILabelProviderListener;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.jface.viewers.TableLayout;
-import org.eclipse.jface.viewers.Viewer;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
 import com.netifera.platform.tools.options.MultipleStringOption;
 
 public class MultipleStringOptionWidget extends OptionWidget {
 
-	private CheckboxTableViewer viewer;
+	private List<Button> buttons;
 	
 	public MultipleStringOptionWidget(Composite parent, FormToolkit toolkit, final MultipleStringOption option) {
 		super(parent, toolkit, option);
 		
 		Composite area = toolkit.createComposite(parent);
 		area.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, false));
-		area.setLayout(new GridLayout(1,false));
+		GridLayout layout = new GridLayout(1,false);
+		layout.verticalSpacing = 0;
+		area.setLayout(layout);
 		area.setToolTipText(option.getDescription());
 		
 		Label label = toolkit.createLabel(area, option.getLabel());
 		label.setLayoutData(new GridData(GridData.BEGINNING, GridData.CENTER, false, false));
 		label.setToolTipText(option.getDescription());
 
-		Table table = new Table(area, SWT.CHECK | SWT.BORDER | SWT.MULTI | SWT.FULL_SELECTION);
-		table.setHeaderVisible(false);
-		table.setLinesVisible(true);
-
-		table.setData(FormToolkit.KEY_DRAW_BORDER, FormToolkit.TEXT_BORDER);
-		table.setLayoutData(new GridData(GridData.FILL, GridData.CENTER, true, false));
-		table.setToolTipText(option.getDescription());
-
-//		TableColumn col1 = new TableColumn(table, SWT.NONE);
-//		col1.setText("Name");
-//		TableColumn col2 = new TableColumn(table, SWT.NONE);
-//		col2.setText("Size");
-
-		TableLayout layout = new TableLayout();
-		layout.addColumnData(new ColumnWeightData(33));
-		layout.addColumnData(new ColumnWeightData(33));
-		table.setLayout(layout);
-
-		viewer = new CheckboxTableViewer(table);
-		viewer.setContentProvider(new IStructuredContentProvider() {
-			MultipleStringOption option;
-			public Object[] getElements(Object inputElement) {
-				return option == null ? new String[0] : option.getPossibleValues();
-			}
-			public void dispose() {
-				// TODO Auto-generated method stub
-			}
-			public void inputChanged(Viewer viewer, Object oldInput,
-					Object newInput) {
-				option = (MultipleStringOption) newInput;
-			}
-		});
-		viewer.setLabelProvider(new ITableLabelProvider() {
-			public Image getColumnImage(Object element, int columnIndex) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			public String getColumnText(Object element, int columnIndex) {
-				return element.toString();
-			}
-			public void addListener(ILabelProviderListener listener) {
-				// TODO Auto-generated method stub
-			}
-			public void dispose() {
-				// TODO Auto-generated method stub
-			}
-			public boolean isLabelProperty(Object element, String property) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			public void removeListener(ILabelProviderListener listener) {
-				// TODO Auto-generated method stub
-			}
-		});
-		
-		viewer.addSelectionChangedListener(new ISelectionChangedListener() {
-			public void selectionChanged(SelectionChangedEvent event) {
-//				IStructuredSelection sel = (IStructuredSelection) event.getSelection();
-//				System.out.println(sel.size() + " items selected, " + viewer.getCheckedElements().length + " items checked");
-				setOptionValue();
-				modified();
-			}
-		});
-		viewer.setInput(option);
-		
+		buttons = new ArrayList<Button>();
+		for (String value: option.getPossibleValues()) {
+			Button button = toolkit.createButton(area, value, SWT.CHECK);
+			button.setSelection(false);
+			button.addSelectionListener(new SelectionListener() {
+				public void widgetDefaultSelected(SelectionEvent arg0) {
+					setOptionValue();
+					modified();
+				}
+				public void widgetSelected(SelectionEvent arg0) {
+					setOptionValue();
+					modified();
+				}
+			});
+			
+			if (option.isFixed())
+				button.setGrayed(true);
+			
+			buttons.add(button);
+		}
+				
 		toolkit.paintBordersFor(area);
-
-		if (option.isFixed())
-			table.setEnabled(false);
 	}
 	
 	public MultipleStringOption getOption() {
@@ -116,14 +66,10 @@ public class MultipleStringOptionWidget extends OptionWidget {
 	}
 	
 	public void setOptionValue() {
-//		if (index < 0) {
-//			getOption().setToDefault();
-//		} else {
-			Object[] checkedElements = viewer.getCheckedElements();
-			String[] checkedStrings = new String[checkedElements.length];
-			for (int i=0; i<checkedElements.length; i++)
-				checkedStrings[i] = (String) checkedElements[i];
-			getOption().setValue(checkedStrings);
-//		}
+		List<String> checkedStrings = new ArrayList<String>();
+		for (Button button: buttons)
+			if (button.getSelection())
+				checkedStrings.add(button.getText());
+		getOption().setValue(checkedStrings.toArray(new String[checkedStrings.size()]));
 	}
 }
