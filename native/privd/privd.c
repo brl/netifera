d1e18e1c60a6af15a18be9e4a13a0d9801c45a74
brl <bruce@netifera.com>
2010-03-03 17:00:18 -0500

    Command line options for configuring and installing privd.

--- native/privd/privd.c
+++ native/privd/privd.c
@@ -5,22 +5,40 @@
 #include <string.h>
 #include <syslog.h>
 #include <errno.h>
+#include <limits.h>
 #include "privd.h"
 
 static void run_loop(struct privd_instance *);
 static char *create_abort_syslog_format(const char *orig_format);
 
+#define INSTALLATION_PATH "/usr/local/bin/netifera_privd"
 
 int
 main(int argc, char **argv)
 {
+	char path_buffer[PATH_MAX];
+
+	if((argc == 2) && strcmp(argv[1], "--install") == 0) {
+		path_buffer[0] = 0;
+		if(realpath(argv[0], path_buffer) == NULL) {
+			fprintf(stderr, "Failed to resolve source path of binary at %s : %s\n", path_buffer, strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		install_privd(INSTALLATION_PATH, path_buffer);
+		return 0;
+	}
+
+	if((argc == 3) && strcmp(argv[1], "--config") == 0) {
+		generate_config(argv[2]);
+		return 0;
+	}
+
 	struct privd_instance *privd = calloc(1, sizeof(struct privd_instance));
-	privd->debug_flag = 1;
+	privd->debug_flag = 0;
 	initialize(privd);
 	run_loop(privd);
 	return 0;
 }
-
 
 static void
 run_loop(struct privd_instance *privd) {
