9d6326eed6910c91953190a512cc1c361d9b5407
brl <bruce@netifera.com>
2010-03-03 16:59:53 -0500

    Password authentication implemented.  Permission checks on configuration file enforced.

--- native/privd/authentication.c
+++ native/privd/authentication.c
@@ -15,6 +15,17 @@ static char *get_config_path(struct privd_instance *);
 static FILE *open_config(struct privd_instance *);
 static int check_config_permissions(struct privd_instance *, int);
 
+int
+authenticate(struct privd_instance *privd, const char *password)
+{
+	if(privd->auth_hash == NULL)
+		return 0;
+	if(strcmp(privd->auth_hash, "DISABLED") == 0)
+		return 1;
+	char *result = bcrypt(password, privd->auth_hash);
+	return (strcmp(result, privd->auth_hash)) ? (0) : (1);
+}
+
 void
 read_authentication_data(struct privd_instance *privd)
 {
@@ -24,12 +35,12 @@ read_authentication_data(struct privd_instance *privd)
 		return;
 	char line[128];
 	if(fgets(line, sizeof(line), fp) == NULL) {
-		send_startup(privd, PRIVD_STARTUP_CONFIG_BAD_DATA, "Error reading daemon configuration");
+		send_startup(privd, PRIVD_STARTUP_CONFIG_BAD_DATA, "Error reading daemon configuration.");
 		return;
 	}
 	char *p = rindex(line, '\n');
 	if(p == NULL) {
-		send_startup(privd, PRIVD_STARTUP_CONFIG_BAD_DATA, "Badly formed config file. No newline found");
+		send_startup(privd, PRIVD_STARTUP_CONFIG_BAD_DATA, "Badly formed config file. No newline found.");
 		return;
 	}
 	*p = 0;
@@ -58,14 +69,14 @@ get_config_path(struct privd_instance *privd)
 	uid_t uid = getuid();
 	struct passwd *pw = getpwuid(uid);
 	if(pw == NULL || pw->pw_dir == NULL) {
-		send_startup(privd, PRIVD_STARTUP_CONFIG_NOT_FOUND, "Failed to look up home directory in password file");
+		send_startup(privd, PRIVD_STARTUP_CONFIG_NOT_FOUND, "Failed to look up home directory in password file.");
 		return NULL;
 	}
 
 	static char buffer[PATH_MAX + 1];
 
 	if(snprintf(buffer, sizeof(buffer), "%s/.privd/config", pw->pw_dir) > sizeof(buffer)) {
-		send_startup(privd, PRIVD_STARTUP_CONFIG_NOT_FOUND, "Overflow creating configuration path");
+		send_startup(privd, PRIVD_STARTUP_CONFIG_NOT_FOUND, "Overflow creating configuration path.");
 		return NULL;
 	}
 	return buffer;
@@ -102,21 +113,21 @@ check_config_permissions(struct privd_instance *privd, int fd)
 {
 	struct stat st;
 	if(fstat(fd, &st) < 0) {
-		// XXX
+		send_startup(privd, PRIVD_STARTUP_CONFIG_BAD_PERMS, "Failed to fstat() config file.");
 		return 0;
 	}
 	if(st.st_uid != 0) {
-		// XXX
+		send_startup(privd, PRIVD_STARTUP_CONFIG_BAD_PERMS, "Incorrect ownership of config file.  Must be owned by root not uid=%d", st.st_uid);
 		return 0;
 	}
 	mode_t mask = S_IWGRP | S_IRGRP | S_IWOTH | S_IROTH;
 	if(st.st_mode & mask) {
-		// XXX
+		send_startup(privd, PRIVD_STARTUP_CONFIG_BAD_PERMS, "Incorrect permissions on config file.  Must only be readable and writable by owner");
 		return 0;
 	}
 
 	if(st.st_nlink != 1) {
-		// XXX
+		send_startup(privd, PRIVD_STARTUP_CONFIG_BAD_PERMS, "Unacceptable hard links exist to configuration file.");
 		return 0;
 	}
 	return 1;
